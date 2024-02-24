c2a70335d6507f271bbacfa19f583c25842a1a24
Robert Castro <oneroberth@gmail.com>
2024-02-24 10:22:28 -0500

    Update main

--- user-registration-service/ping_service_log.py
+++ user-registration-service/ping_service_log.py
@@ -1,12 +1,11 @@
 from celery import Celery
 
-celery = Celery('ping_logs', broker='redis://redis:6379/0')
+celery = Celery('ping_logs', broker='redis://localhost:6379/1')
 
 def insert_ping_in_db(ping_id, date):
-    file_name = "ping_logs.txt"
-    with open(file_name, "a") as file:
+    file_name = "ping_logs.csv"
+    with open(file_name, mode='a', encoding='utf-8') as file:
         file.write(ping_id+","+date+"\n")
-
 
 @celery.task
 def registrar_ping_recibido(ping_id, ping_datetime):
